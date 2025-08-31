a5f2360438b302b7bed29da4bdf88c82e2f3797f
[sCRiPTz-TEAM] <scriptzteam@users.noreply.github.com>
2025-08-31 13:27:47 +0200

    Update azirevpn.sh

--- azirevpn.sh
+++ azirevpn.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
-# API endpoint
 API_URL="https://api.azirevpn.com/v3/locations"
 
-# Fetch data, parse JSON with jq, format with awk
+# Fetch location info
 curl -s "$API_URL" \
   | jq -r '.locations[] | [.name, .city, .country, .pool, .pubkey // ""] | @tsv' \
-  | awk -F'\t' '
-    BEGIN {
-        # Print table header
-        printf("| %-10s | %-20s | %-15s | %-25s | %-44s |\n", "NAME", "CITY", "COUNTRY", "POOL", "PUBKEY")
-        printf("|------------|----------------------|-----------------|---------------------------|--------------------------------------------|\n")
-    }
-    {
-        # Print table rows
-        printf("| %-10s | %-20s | %-15s | %-25s | %-44s |\n", $1, $2, $3, $4, $5)
-    }
-' > README.md
+  | while IFS=$'\t' read -r NAME CITY COUNTRY POOL PUBKEY; do
+      # Resolve pool hostname to IP (may return multiple IPs)
+      IPS=$(dig +short "$POOL" | paste -sd " , ")
+      printf "| %-10s | %-20s | %-15s | %-25s | %-44s | %-40s |\n" \
+        "$NAME" "$CITY" "$COUNTRY" "$POOL" "$PUBKEY" "$IPS"
+  done > README.md
+
+# Add table header manually
+sed -i '1i| NAME       | CITY                 | COUNTRY         | POOL                      | PUBKEY                                     | IP(s)                                   |' README.md
+sed -i '2i|------------|----------------------|-----------------|---------------------------|--------------------------------------------|-----------------------------------------|' README.md
